3e1f51467e768148ecfec9357551470e8c366a65
Ahmad Raza <ahmadsajidmughal357@gmail.com>
2025-08-02 23:29:26 +0500

    Fix Filter Issue by wraping in Suspence component

--- app/issues/IssuesActionBar.tsx
+++ app/issues/IssuesActionBar.tsx
@@ -1,12 +1,14 @@
 import { Button, Flex } from '@radix-ui/themes'
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
 import FilterIssue from './FilterIssue'
 
 const IssuesActionBar = () => {
     return (
         <Flex mb='5' justify='between'>
-            <FilterIssue />
+            <Suspense fallback={<div>Loading filters...</div>}>
+                <FilterIssue />
+            </Suspense>
             <div>
                 <Button><Link href="/issues/new">New Issue</Link></Button>
             </div>
