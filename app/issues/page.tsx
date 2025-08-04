a3ab579bd965b1326820ccf085c3baa629edfd6e
Ahmad Raza <ahmadsajidmughal357@gmail.com>
2025-08-04 17:59:08 +0500

    Change pagination pageSize

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -23,7 +23,7 @@ const IssuePage = async ({ searchParams }: Props) => {
 
 
     const page = parseInt(searchParams.page) || 1
-    const pageSize = 10
+    const pageSize = 6
 
     const issues = await prisma.issue.findMany({
         where: {
